14668a7818661e97901d9fd414f3213f46925666
김현수 <fogy431@gmail.com>
2025-11-03 23:14:58 +0900

    style(model): 불필요 출력 공백 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private final static String REQUEST_MONEY_MESSAGE = " 구입금액을 입력해 주세요.";
-    private final static String REQUEST_WIN_NUMBERS_MESSAGE = " 당첨 번호를 입력해 주세요.";
-    private final static String REQUEST_BONUS_NUMBER_MESSAGE = " 보너스 번호를 입력해 주세요.";
+    private final static String REQUEST_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private final static String REQUEST_WIN_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final static String REQUEST_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static String getUserInputBuyMoney() {
         System.out.println(REQUEST_MONEY_MESSAGE);
